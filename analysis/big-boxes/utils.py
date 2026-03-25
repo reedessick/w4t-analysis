cb66e63c9859a04ad3a03ffa15fa582b9ab1719b
Reed Essick <reed.essick@gmail.com>
2026-03-25 09:01:05 -0400

    attempt to sample SL ansatz

--- analysis/big-boxes/utils.py
+++ analysis/big-boxes/utils.py
@@ -21,7 +21,7 @@ from numpyro.diagnostics import effective_sample_size
 
 numpyro.enable_x64() # improve default numerical precision
 
-from w4t.utils.infer import thin
+from w4t.utils.infer import (thin, scaling_exponent_ansatz, _sample_sea_xcb_prior)
 
 #-------------------------------------------------
 
@@ -144,6 +144,42 @@ def load_structure_function_samples(path, verbose=False):
 
     return posterior, prior, data, meta
 
+#------------------------
+
+def write_scaling_exponent_samples(
+        path,
+        posterior,
+        prior,
+        verbose=False,
+        **meta
+    ):
+    if verbose:
+        print('writing samples to: '+path)
+
+    with h5py.File(path, 'w') as obj:
+        for k, v in meta.items():
+            obj.attrs.create(k, v)
+
+        for grp, data in [
+                (obj.create_group('posterior'), posterior),
+                (obj.create_group('prior'), prior),
+            ]:
+            for k, v in data.items():
+                grp.create_dataset(k, data=v)
+
+#---
+
+def load_scaling_exponent_samples(path, verbose=False):
+    if verbose:
+        print('loading structure function samples from: '+path)
+
+    with h5py.File(path, 'r') as obj:
+        posterior = dict((k, v[:]) for k, v in obj['posterior'].items())
+        prior = dict((k, v[:]) for k, v in obj['prior'].items())
+        meta = dict(obj.attrs.items())
+
+    return posterior, prior, meta
+
 #-------------------------------------------------
 
 def structure_function_ansatz(scales, amp, xi, s1, b1, n1, s2, b2, n2, s3, b3, n3, ref_scale=0.5):
@@ -154,6 +190,9 @@ def structure_function_ansatz(scales, amp, xi, s1, b1, n1, s2, b2, n2, s3, b3, n
 
 def logarithmic_derivative_structure_function_ansatz(scales, amp, xi, s1, b1, n1, s2, b2, n2, s3, b3, n3, ref_scale=0.5):
     return xi + b1/(1+(scales/s1)**(-n1)) + b2/(1+(scales/s2)**(-n2)) + b3/(1+(scales/s3)**(-n3))
+
+def averaged_logarithmic_derivative_ansatz(low, high, *args):
+    return np.log(structure_function_ansatz(high, *args) / structure_function_ansatz(low, *args)) / np.log(high/low)
 
 #------------------------
 
@@ -345,6 +384,189 @@ def sample_structure_function_ansatz(
             print('computing likelihood at samples')
 
         posterior.update(numpyro.infer.log_likelihood(sample_posterior, posterior, mom))
+
+        if Posterior is None:
+            Posterior = dict((k, [v]) for k, v in posterior.items())
+
+        else:
+            for k, v in posterior.items():
+                Posterior[k].append(v)
+
+    assert Posterior is not None, 'no seeds succeeded for the posterior!'
+
+    #---
+
+    Posterior = dict((k, np.concatenate(tuple(v))) for k, v in Posterior.items())
+    Prior = dict((k, np.concatenate(tuple(v))) for k, v in Prior.items())
+
+    if verbose:
+        print('\n>>> retained a total of %d prior samples' % len(list(Prior.values())[0]))
+        print('>>> retained a total of %d posterior samples\n' % len(list(Posterior.values())[0]))
+
+    # return
+    return Posterior, Prior
+
+#-------------------------------------------------
+
+def simple_sample_scaling_exponent_ansatz(
+        indexes,
+        samples,
+        ref_scale,
+        num_warmup=DEFAULT_NUM_WARMUP,
+        num_samples=DEFAULT_NUM_SAMPLES,
+        num_retained=DEFAULT_NUM_RETAINED,
+        seed=[DEFAULT_SEED],
+        verbose=False,
+        num_segs=1,
+        xi_prior_mean=0.0,
+        xi_prior_stdv=3.0,
+        **prior_kwargs
+    ):
+    """sample the distribution of scaling exponents using a KDE model of marginal likelihoods
+    """
+    if verbose:
+        print('defining model')
+
+    num_indexes = len(indexes)
+    
+    # computing KDE models of averaged scaling exponents
+    grids = []
+    kdes = []
+
+    for ind, i in enumerate(indexes):
+        if verbose:
+            print('    computing KDE for xi(index=%d) with %d samples' % (i, len(samples[i]['amp'])))
+    
+        samp = averaged_logarithmic_derivative_ansatz(
+            ref_scale[0],
+            ref_scale[1],
+            samples[i]['amp'],
+            samples[i]['xi'],
+            samples[i]['sl'],
+            samples[i]['bl'],
+            samples[i]['nl'],
+            samples[i]['sh'],
+            samples[i]['bh'],
+            samples[i]['nh'],
+        )
+
+        # NOTE! this assumes a default prior, which could be fragile
+        # prior for xi that we're going to undo
+        # in practice, xi is constrained well compared to the mean so that weights are all about the same
+        w = 1./(np.exp(0.5*(samples[i]['xi']-xi_prior_mean)**2/3**2) / (2*np.pi * xi_prior_stdv**2)**0.5)
+        w /= np.sum(w)
+
+        ### simple Gaussian approximation
+        m = np.sum(w*samp)             # sample mean
+        s = np.sum(w*(samp-m)**2)**0.5 # sample stdv (sorta)
+
+        ### KDE of samples
+        kde_stdv = 1.06 * s / len(samp)**0.2 # rule of thumb for optimal KDE bandwidth w/r/t IMSE
+
+        grid = np.min(samp), np.max(samp)
+        d = 0.5*(grid[1]-grid[0])
+        grid = np.linspace(grid[0]-d, grid[1]+d, 101)
+        kde = np.sum(
+            [W*np.exp(-0.5*(S-grid)**2/kde_stdv**2)/(2*np.pi*kde_stdv**2)**0.5 for W, S in zip(w, samp)],
+            axis=0,
+        )
+
+        grids.append(grid)
+        kdes.append(kde)
+
+    grids = np.array(grids, dtype=float)
+    kdes = np.array(kdes, dtype=float)
+
+    # finish defining model
+
+    def sample_posterior(**prior_kwargs):
+        x, C0, beta = _sample_sea_xcb_prior(**prior_kwargs)                 # sample from prior
+        xi = scaling_exponent_ansatz(indexes, x, C0, beta)  # compute means
+        for ind, i in enumerate(indexes): # this shouldn't be too bad, but may need to vmap
+            numpyro.factor('data_%d'%i, jnp.log(jnp.interp(xi[ind], grids[ind], kdes[ind])))
+
+    #---
+
+    Prior = None
+
+    for s in seed:
+
+        # run the sampler
+
+        if verbose:
+            print('running sampler for prior with seed=%d for %d warmup and %d samples' % \
+                (s, num_warmup, num_samples))
+
+        try:
+            mcmc = MCMC(
+                NUTS(_sample_sea_xcb_prior, init_strategy=init_to_value(values=init_xcb_values)),
+                num_warmup=num_warmup,
+                num_samples=num_samples,
+            )
+            mcmc.run(random.PRNGKey(s), **prior_kwargs)
+        except:
+            if verbose:
+                print('>>> sampler failed!')
+            continue
+
+        if verbose:
+            mcmc.print_summary(exclude_deterministic=False)
+
+        prior = mcmc.get_samples()
+        prior = thin(num_samples, prior, prior.keys(), num_segs=num_segs, verbose=verbose)
+
+        if num_retained < np.inf:
+            if verbose:
+                print('retaining the final %d samples' % num_retained)
+            prior = dict((key, val[-num_retained:]) for key, val in prior.items())
+
+        if Prior is None:
+            Prior = dict((k, [v]) for k, v in prior.items())
+
+        else:
+            for k, v in prior.items():
+                Prior[k].append(v)
+
+    assert Prior is not None, 'no seeds succeeded for the prior!'
+
+    #---
+
+    Posterior = None
+
+    for s in seed:
+
+        if verbose:
+            print('running sampler for posterior with seed=%d for %d warmup and %d samples' % \
+                (s, num_warmup, num_samples))
+
+        try:
+            mcmc = MCMC(
+                NUTS(sample_posterior, init_strategy=init_to_value(values=init_xcb_values)),
+                num_warmup=num_warmup,
+                num_samples=num_samples,
+            )
+            mcmc.run(random.PRNGKey(s), **prior_kwargs)
+        except:
+            if verbose:
+                print('>>> sampler failed!')
+            continue
+
+        if verbose:
+            mcmc.print_summary(exclude_deterministic=False)
+
+        posterior = mcmc.get_samples()
+        posterior = thin(num_samples, posterior, posterior.keys(), num_segs=num_segs, verbose=verbose)
+
+        if num_retained < np.inf:
+            if verbose:
+                print('retaining the final %d samples' % num_retained)
+            posterior = dict((key, val[-num_retained:]) for key, val in posterior.items())
+
+        # record the likelihood of each sample
+        if verbose:
+            print('computing likelihood at samples')
+
+        posterior.update(numpyro.infer.log_likelihood(sample_posterior, posterior, **prior_kwargs))
 
         if Posterior is None:
             Posterior = dict((k, [v]) for k, v in posterior.items())
